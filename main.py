3d91ae57bacceb5bce7c17faea3ede8e53b45f73
Vladimir <arazkov@ya.ru>
2021-05-24 00:07:15 +0300

    V 010 completely rebuilt the application interface

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from kivy.uix.accordion import Accordion, AccordionItem
+from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 from kivy.core.window import Window
 
-#Window.size = (480, 853)
+Window.size = (480, 853)
 
-class Container(Accordion):
-    pass
+#def qw():
+        #stat = ''
+        #sex = ''
+        #age = ''
+        #exersize = ''
+        #rezult = f"{stat}\n{sex}\n{age}\n{exersize}"
+        #return rezults
+data = {}
+
+class Container(BoxLayout):
+
+
+    def qw(self, param):
+        #self.stat = ''
+        #self.sex = ''
+        #self.age = ''
+        #self.exersize = ''
+        #self.rezult = f"{self.stat}\n{self.sex}\n{self.age}\n{self.exersize}"
+        data.update(param)
+        self.rez.text = f"{data['stat'] if data['stat'] else ''}\n{data['sex'] if 'sex' in data else ''}"
+        #return self.rezult
+        #pass
 
 class MvdSportApp(App):
     def build(self):
