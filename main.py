7cefd01b8c720c1e170c0a42656fd69c4280badb
Vladimir <arazkov@ya.ru>
2021-05-24 18:45:41 +0300

    V 011 edit age and exersize param

--- main.py
+++ main.py
@@ -4,28 +4,14 @@ from kivy.core.window import Window
 
 Window.size = (480, 853)
 
-#def qw():
-        #stat = ''
-        #sex = ''
-        #age = ''
-        #exersize = ''
-        #rezult = f"{stat}\n{sex}\n{age}\n{exersize}"
-        #return rezults
 data = {}
 
 class Container(BoxLayout):
 
 
     def qw(self, param):
-        #self.stat = ''
-        #self.sex = ''
-        #self.age = ''
-        #self.exersize = ''
-        #self.rezult = f"{self.stat}\n{self.sex}\n{self.age}\n{self.exersize}"
         data.update(param)
-        self.rez.text = f"{data['stat'] if data['stat'] else ''}\n{data['sex'] if 'sex' in data else ''}"
-        #return self.rezult
-        #pass
+        self.rez.text = f"{data['stat'] if 'stat' in data else ''}\n{data['sex'] if 'sex' in data else ''}\n{data['age'] if 'age' in data else ''}\n{data['exersize'] if 'exersize' in data else ''}"
 
 class MvdSportApp(App):
     def build(self):
