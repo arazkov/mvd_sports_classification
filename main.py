c4d8fc9f75830b4e578db39acbab6707771dcaa6
Vladimir_pc <arazkov@ya.ru>
2021-05-30 23:23:44 +0300

    V 016 add women json file and edit code to calculate women balls

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ data = {
 }
 
 
-def calculate_balls(exercise, result):
-    with open('sport_data.json', 'r') as js:
+def calculate_balls(sex, exercise, result):
+    if sex == 'male':
+        json_faile = 'balls_men.json'
+    else:
+        json_faile = 'balls_women.json'
+    with open(json_faile, 'r') as js:
         balls = json.load(js)
     ind = balls['body'][0].index(exercise)
     for i in balls['body']:
@@ -26,8 +30,12 @@ def calculate_balls(exercise, result):
 class Container(BoxLayout):
     def qw(self, param):
         data.update(param)
+        if data['sex'] == 'male':
+            self.exer.values = ["Бег", "Отжимания", "Подтягивания", "Гиря"]
+        elif data['sex'] == 'female':
+            self.exer.values = ["Бег", "Отжимания", "Прес"]
         if all([data['stat'], data['sex'], data['age'], data['exercise'], data['result']]):
-            self.rez.text = f"Вы набрали\n [color=ff3333]       {calculate_balls(data['exercise'], data['result'])}\n[/color] [color=3333ff][/color]    баллов"
+            self.rez.text = f"Вы набрали\n [color=ff3333]       {calculate_balls(data['sex'], data['exercise'], data['result'])}\n[/color] [color=3333ff][/color]    баллов"
 
 
 class MvdSportApp(App):
