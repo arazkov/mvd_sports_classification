482dabe88f1f15b04586a95ddecac88d7b166cec
Vladimir_pc <arazkov@ya.ru>
2021-05-30 21:51:56 +0300

    V 015_3 edit same bugs from PyCharm on pep8

--- main.py
+++ main.py
@@ -9,29 +9,31 @@ data = {
     'stat': None,
     'sex': None,
     'age': None,
-    'exersize': None,
-    'rezult': None
-    }
+    'exercise': None,
+    'result': None
+}
+
+
+def calculate_balls(exercise, result):
+    with open('sport_data.json', 'r') as js:
+        balls = json.load(js)
+    ind = balls['body'][0].index(exercise)
+    for i in balls['body']:
+        if i[ind] == result:
+            return i[0]
 
 
 class Container(BoxLayout):
-    def calculate_balls(self, exersize, rezult):
-        with open('sport_data.json','r') as js:
-            balls = json.load(js)
-        ind = balls['body'][0].index(exersize)
-        for i in balls['body']:
-            if i[ind] == rezult:
-                return i[0]
-
-
     def qw(self, param):
         data.update(param)
-        if all([data['stat'], data['sex'], data['age'], data['exersize'], data['rezult']]):
-            self.rez.text = f"Вы набрали\n [color=ff3333]       {self.calculate_balls(data['exersize'], data['rezult'])}\n[/color] [color=3333ff][/color]    баллов"
+        if all([data['stat'], data['sex'], data['age'], data['exercise'], data['result']]):
+            self.rez.text = f"Вы набрали\n [color=ff3333]       {calculate_balls(data['exercise'], data['result'])}\n[/color] [color=3333ff][/color]    баллов"
+
 
 class MvdSportApp(App):
     def build(self):
         return Container()
+
 
 if __name__ == '__main__':
     MvdSportApp().run()
