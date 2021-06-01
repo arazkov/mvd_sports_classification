223279d797693cb7b9bd81eb231c5c5f6943ff72
Vladimir_pc <arazkov@ya.ru>
2021-06-02 00:00:16 +0300

    V 018 edit to view from man and women different age param and main logic function delete from three functions

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ import json
 Window.size = (480, 853)
 
 data = {
-    'stat': None,
-    'sex': None,
-    'age': None,
-    'exercise': None,
-    'result': None
+    'stat': 0,
+    'sex': 0,
+    'age': 0,
+    'exercise': 0,
+    'result': 0
 }
 
 
 def calculate_balls(sex, exercise, result):
     if sex == 'male':
-        json_faile = 'balls_men.json'
+        json_file = 'balls_men.json'
     else:
-        json_faile = 'balls_women.json'
-    with open(json_faile, 'r') as js:
+        json_file = 'balls_women.json'
+    with open(json_file, 'r') as js:
         balls = json.load(js)
     ind = balls['body'][0].index(exercise)
     for i in balls['body']:
@@ -27,22 +27,49 @@ def calculate_balls(sex, exercise, result):
             return i[0]
 
 
+def shows_the_remaining_exercises(balls, stat, sex):
+    pass
+
+
 class Container(BoxLayout):
-    def qw(self, param):
-        data.update(param)
+    def select_params_to_view(self):
         if data['sex'] == 'male':
             self.exer.values = ["Бег", "Отжимания", "Подтягивания", "Гиря"]
+            self.age.values = [
+                'до 25 лет',
+                'от 25 до 30 лет',
+                'от 30 до 35 лет',
+                'от 35 до 40 лет',
+                'от 40 до 45 лет',
+                'от 45 до 50 лет',
+                'от 50 до 55 лет',
+                '55 лет и старше']
         elif data['sex'] == 'female':
             self.exer.values = ["Бег", "Отжимания", "Прес"]
+            self.age.values = [
+                'до 25 лет',
+                'от 25 до 30 лет',
+                'от 30 до 35 лет',
+                'от 35 до 40 лет',
+                'от 40 до 45 лет',
+                '45 лет и старше']
+        if data['exercise'] == 'run_10_10':
+            self.ti.input_filter = 'float'
+        else:
+            self.ti.input_filter = 'int'
+
+    def view_result(self):
+        try:
+            self.rez.text = f"Вы набрали\n [color=ff3333]       {calculate_balls(data['sex'], data['exercise'], data['result'])}\n[/color] [color=3333ff][/color]    баллов"
+        except ValueError:
+            self.rez.text = 'ВВЕДИТЕ КОРЕКТНЫЕ ДАННЫЕ!'
+        print(data)
+
+    def qw(self, param):
+        data.update(param)
+        self.select_params_to_view()
         if all([data['stat'], data['sex'], data['age'], data['exercise'], data['result']]):
-            try:
-                if data['exercise'] == 'run_10_10':
-                    self.ti.input_filter = 'float'
-                else:
-                    self.ti.input_filter = 'int'
-                self.rez.text = f"Вы набрали\n [color=ff3333]       {calculate_balls(data['sex'], data['exercise'], data['result'])}\n[/color] [color=3333ff][/color]    баллов"
-            except:
-                self.rez.text = 'ВВЕДИТЕ КОРЕКТНЫЕ ДАННЫЕ!'
+            self.view_resulte()
 
 
 class MvdSportApp(App):
